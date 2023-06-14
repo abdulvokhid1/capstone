9305b1e62417c744ac679a32d21eff1b12c0318b
Abdulvokhid Ibrakhimov <ibrakhimov7747a@gmail.com>
2023-06-14 21:42:16 +0900

    new

---
+++ src/utils/auth/data.js
@@ -0,0 +1,11 @@
+export const data = [
+  {
+    role: "admin",
+    password: "admin",
+    email: "admin@gmail.com",
+    userName: "admin",
+    firstName: "Admin",
+    lastName: "abdulvokhid",
+    phone: "01057954998",
+  },
+];
